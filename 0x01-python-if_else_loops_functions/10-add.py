573c992358950efb59568ea2c32b94e4d78f46b6
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2023-12-02 11:22:47 +0300

    Add files via upload

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Aklil dereje
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
+
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author -Aklil Dereje
+
+def pow(a, b):
+    return (a ** b)
