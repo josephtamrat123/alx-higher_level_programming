a211b23df7c26bf6c7e994735c73d3c9bb32556e
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2024-02-21 14:21:43 +0300

    Add files via upload

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,8 @@
-"
-This module defines `print_square`
-The function prints a square
-"""
-
-
+#!/usr/bin/python3
 def print_square(size):
-    """prints a square with size, `size`
-    Args:
-        size (int)
-    Raises:
-        TypeError: size must be an integer
-        ValueError: size must be >= 0
-    """
-
-    if type(size) != int:
-        raise TypeError('size must be an integer')
-    elif size < 0:
-        raise ValueError('size must be >= 0')
-
-    for x in range(size):
-        print('#' * size)
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for index in range(size):
+        print("x" * size)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines `text_indentation`
-The function prints a text with 2 new lines after each of
-these characters: ., ? and :
-"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """splits a text into lines along "?", ":", "." followed by 2 new lines
-    Args:
-        text: input string
-    Returns:
-        No return
-    Raises:
-        TypeError: If text is not a string
-    """
-
-    if type(text) is not str:
+    """text indent"""
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    flag = 0
-    a = 0
-    while a < len(text):
-        if flag == 0:
-            if text[a] == ' ':
-                a += 1
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if text[a] == '?' or text[a] == '.' or text[a] == ':':
-                print(text[a])
-                print()
-                flag = 0
-            else:
-                if text[a + 1] == '\n':
-                     a += 1
-                     continue 
-                print(text[a], end="")
-           a +=1 
+
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
