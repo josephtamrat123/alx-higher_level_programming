f124d7b1c0e28f93be5ca59276db10ec76b292c6
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2024-03-29 23:30:07 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
+
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import Request, urlopen
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request("https://alx-intranet.hbtn.io/status")
-    with urlopen(req) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
