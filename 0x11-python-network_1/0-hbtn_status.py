eeb977d192525410d55c14aae33f0a55584f7b58
josephtamrat123 <141209846+josephtamrat123@users.noreply.github.com>
2024-03-29 23:30:43 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,6 @@
-
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-
-
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
